7de91c3e4e62131c064f0a7fec59818dca8acc18
Miłosz Meller <meller.milosz@gmail.com>
2021-09-10 10:10:29 +0200

    Move managers to separate files

---
+++ backend/managers/connection_manager.py
@@ -0,0 +1,28 @@
+from typing import Dict, List, Optional
+
+from fastapi import WebSocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[int, List[WebSocket]] = {}
+
+    async def connect(self, game_id: int, websocket: WebSocket):
+        await websocket.accept()
+        if game_id not in self.active_connections:
+            self.active_connections[game_id] = []
+        self.active_connections[game_id].append(websocket)
+
+    def disconnect(self, game_id: int, websocket: WebSocket):
+        self.active_connections[game_id].remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, game_id: int, message: str, skip: Optional[WebSocket]):
+        for connection in self.active_connections[game_id]:
+            if connection != skip:
+                await connection.send_text(message)
+
+    def connection_count(self, game_id: int):
+        return len(self.active_connections[game_id])
---
+++ backend/managers/game_manager.py
@@ -0,0 +1,27 @@
+from typing import Dict, List, Tuple
+
+from fastapi import WebSocket
+
+from .connection_manager import ConnectionManager
+
+
+class GameManager:
+    def __init__(self, connection_manager: ConnectionManager):
+        self.connection_manager = connection_manager
+        self.games: Dict[int, List[Tuple[WebSocket, str]]] = {}
+
+    async def move(self, game_id: int, player: WebSocket, move: str):
+        if game_id not in self.games:
+            self.games[game_id] = []
+        self.games[game_id].append((player, move))
+        if len(self.games[game_id]) == 2:
+            await self.resolve_game(game_id)
+
+    async def resolve_game(self, game_id: int):
+        for (player, move) in self.games[game_id]:
+            message = f"Opponent played {move}"
+            await self.connection_manager.broadcast(game_id, message, skip=player)
+
+        # TODO: declare winner
+
+        self.games.pop(game_id)
