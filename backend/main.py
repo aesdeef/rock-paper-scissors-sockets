0455fd708d5a50fb26017f379685cd34535de3dd
Miłosz Meller <meller.milosz@gmail.com>
2021-09-16 10:08:26 +0200

    Add move validation

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 from managers.connection_manager import ConnectionManager
@@ -8,6 +10,12 @@ connection_manager = ConnectionManager()
 game_manager = GameManager(connection_manager)
 
 
+class Move(str, Enum):
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+
+
 @app.websocket("/{game_id}")
 async def single_game(websocket: WebSocket, game_id: int):
     await connection_manager.connect(game_id, websocket)
@@ -15,14 +23,24 @@ async def single_game(websocket: WebSocket, game_id: int):
         await connection_manager.send_personal_message(
             "Waiting for an opponent", websocket
         )
+    elif connection_manager.connection_count(game_id) == 2:
+        await connection_manager.broadcast(
+            game_id, "Opponent found. Choose rock, paper, or scissors."
+        )
 
     try:
         while True:
             data = await websocket.receive_text()
-            await connection_manager.send_personal_message(
-                f"You played {data}", websocket
-            )
-            await game_manager.move(game_id, websocket, move=data)
+            try:
+                move = Move(data)
+                await connection_manager.send_personal_message(
+                    f"You played {data}", websocket
+                )
+                await game_manager.move(game_id, websocket, move=data)
+            except ValueError:
+                await connection_manager.send_personal_message(
+                    f"Invalid move: {data}", websocket
+                )
     except WebSocketDisconnect:
         connection_manager.disconnect(game_id, websocket)
         await connection_manager.broadcast(game_id, f"Opponent has left the game")
