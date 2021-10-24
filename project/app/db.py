6acf35cd70df91c71de44c5e9dfbfdb10fcf4482
Miłosz Meller <meller.milosz@gmail.com>
2021-10-24 14:17:51 +0200

    Update Dockerfile.prod

--- project/app/db.py
+++ project/app/db.py
@@ -9,7 +9,6 @@ log = logging.getLogger("uvicorn")
 
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
-    # replace with settings
     "apps": {
         "models": {
             "models": ["app.models.tortoise", "aerich.models"],
